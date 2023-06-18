81c858d6f9f8c930861b4cd09c43432e83089bb0
Samuel AKuffo <sammy4real78@gmail.com>
2023-06-18 22:05:50 +0000

    Git clone Practice

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Elvis from './Elvis';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Elvis/>
+      
+    </div>
+  );
+}
+
+export default App;
